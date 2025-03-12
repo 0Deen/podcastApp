783e8f089fbc345f78c0852ee3d967a613039c86
Kiprotich Kipchumba <01kiprotich@gmail.com>
2025-03-12 22:53:45 +0300

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,5 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+//cn is for shadcn
+//we export the imported function to the respective inputs 
